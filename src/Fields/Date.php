02f24acb2f11d61255fcc4de3c3b03531bf74f2f
Gennadiy Sevalnev <gennadiy.sevalnev@mail.ru>
2025-05-26 19:59:23 +0700

    Add Date field

---
+++ src/Fields/Date.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gelion\BitrixOptions\Fields;
+
+class Date extends Text
+{
+    public function view(): string
+    {
+        return <<<HTML
+            <tr>
+                <td>
+                    $this->title
+                    {$this->getHintView()}
+                </td>
+                <td nowrap>
+                    <div id="$this->key-block">
+                        <div class="$this->key-container">
+                            <div class="ui-ctl ui-ctl-textbox {$this->getClassFromProps()}">
+                                {$this->getTagView()}
+                                <input  type="date"
+                                        class="ui-ctl-element"
+                                        name="$this->key"
+                                        value="$this->value"
+                                        placeholder="$this->placeholder"
+                                        autocomplete="$this->autocomplete"
+                                        {$this->getDisabledView()}
+                                        {$this->getReadonlyView()} />
+                            </div>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        HTML;
+    }
+}
+
+
+
